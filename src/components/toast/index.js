7adec6bffaa5b7c365918fb04364d612e961da9d
Humberto Benedito <humbertobenedito@gmail.com>
2023-02-22 12:08:10 +0100

    fix: update toast component

--- src/components/toast/index.js
+++ src/components/toast/index.js
@@ -2,8 +2,6 @@ import { h } from "preact";
 import { useState, useEffect } from "preact/hooks";
 
 function Toast({ type, message, handleClosed }) {
-  console.log(type, message);
-
   const [toast, setToast] = useState("");
 
   useEffect(() => {
@@ -12,7 +10,7 @@ function Toast({ type, message, handleClosed }) {
         setToast(
           <div
             id="toast-success"
-            class="absolute top-20 right-10 flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+            class="fixed top-20 right-10 flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
             role="alert"
           >
             <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
@@ -30,7 +28,9 @@ function Toast({ type, message, handleClosed }) {
                 />
               </svg>
             </div>
-            <div class="ml-3 text-sm text-justify font-bold drop-shadow-[2px_2px_2px_rgba(0,0,0,0.5)]">{message}</div>
+            <div class="ml-3 text-sm text-justify font-bold drop-shadow-[2px_2px_2px_rgba(0,0,0,0.5)]">
+              {message}
+            </div>
             <button
               type="button"
               class="ml-4 -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
@@ -59,7 +59,7 @@ function Toast({ type, message, handleClosed }) {
         setToast(
           <div
             id="toast-warning"
-            class="absolute top-20 right-10 flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+            class="fixed top-20 right-10 flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
             role="alert"
           >
             <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-orange-500 bg-orange-100 rounded-lg dark:bg-orange-700 dark:text-orange-200">
@@ -78,7 +78,9 @@ function Toast({ type, message, handleClosed }) {
               </svg>
               <span class="sr-only">Warning icon</span>
             </div>
-            <div class="ml-3 text-sm text-justify font-bold drop-shadow-[2px_2px_2px_rgba(0,0,0,0.5)]">{message}</div>
+            <div class="ml-3 text-sm text-justify font-bold drop-shadow-[2px_2px_2px_rgba(0,0,0,0.5)]">
+              {message}
+            </div>
             <button
               type="button"
               class="ml-4 -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
@@ -108,7 +110,7 @@ function Toast({ type, message, handleClosed }) {
         setToast(
           <div
             id="toast-danger"
-            class="absolute top-20 right-10 flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+            class="fixed top-20 right-10 flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
             role="alert"
           >
             <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
@@ -127,7 +129,9 @@ function Toast({ type, message, handleClosed }) {
               </svg>
               <span class="sr-only">Error icon</span>
             </div>
-            <div class="ml-3 text-sm text-justify font-bold drop-shadow-[2px_2px_2px_rgba(0,0,0,0.5)]">{message}</div>
+            <div class="ml-3 text-sm text-justify font-bold drop-shadow-[2px_2px_2px_rgba(0,0,0,0.5)]">
+              {message}
+            </div>
             <button
               type="button"
               class="ml-4 -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
